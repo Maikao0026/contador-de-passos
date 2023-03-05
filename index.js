2e11089f422fe19eaedf05e90c788c34869aa7a6
Michael Douglas <maikao0026@gmail.com>
2023-03-05 08:09:04 -0300

    Atualizando nome dos arquivos

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ function contar(){
   const inicio = Number(document.getElementById('inicio').value) 
   const fim = Number(document.getElementById('fim').value)
   const passo = Number(document.querySelector('input#passo').value)
-  const res = document.querySelectorAll('div#res>p')[0]
+  const resposta = document.querySelectorAll('div#resposta>p')[0]
   let contando = ""
    
   if (passo<1) {
     //alert(`ERRO `)
-    res.innerText = `\u{1F610} impossivel contar`;
+    resposta.innerText = `\u{1F610} impossivel contar`;
   }else if (fim < inicio){
     //contagem regressiva
     for (let i = inicio; i >= fim; i -= passo){
       contando += `👉${i}`
-      res.innerText = `\u{1F603} ${contando} 🏁`;
+      resposta.innerText = `\u{1F603} ${contando} 🏁`;
     }
   }else{
     //contagem cresente
     for (let i = inicio; i <= fim; i += passo) {
       contando+=`👉 ${i}`
-      res.innerText = `\u{1F603} ${contando} 🏁`
+      resposta.innerText = `\u{1F603} ${contando} 🏁`
     }
   }
 }
